986b0b3378a121277efdfefaf8c3e0e1bc2554d7
hongggs <hongseoda@gmail.com>
2024-02-26 23:54:59 +0900

    feat: swea 2383 점심식사시간 부분집합을 이용한 구현 문제

--- src/main/java/samsung/Solution_모의_2383_점심식사시간_손홍서.java
+++ src/main/java/samsung/Solution_모의_2383_점심식사시간_손홍서.java
@@ -1,17 +1,34 @@
 package samsung;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-//https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5-BEE6AK0DFAVl&categoryId=AV5-BEE6AK0DFAVl&categoryType=CODE&problemTitle=%EB%AA%A8%EC%9D%98&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=30&pageIndex=1&&&&&&&&&
 public class Solution_모의_2383_점심식사시간_손홍서 {
+    static int N, M;
+    static Stair[] stairs;
+
+    static boolean[] ch;
+    static ArrayList<int[]> people;
     static int ans;
-    static int[][] map;
-    static int D, W, K;
-    public static void main(String[] args) throws Exception {
-        //0. 입력
+    static class Stair {
+        int r, c, len, cnt; // 계단 위치, 계단 길이, 올라가 있는 사람
+        Stair(int r, int c, int len, int cnt) {
+            this.r = r;
+            this.c = c;
+            this.len = len;
+            this.cnt = cnt;
+        }
+    }
+
+    static class Point {
+        int dir, n, len;
+        public Point(int dir, int n, int len) {
+            this.dir = dir;
+            this.n = n;
+            this.len = len;
+        }
+    }
+    public static void main(String[] args) throws Exception{
         BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
@@ -19,82 +36,112 @@ public class Solution_모의_2383_점심식사시간_손홍서 {
 
         for(int t = 1; t <= T; t++) {
             sb.append("#").append(t).append(" ");
-            st = new StringTokenizer(br.readLine());
-            D = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-            map = new int[D][W];
-            for(int i = 0; i < D; i++) {
+            N = Integer.parseInt(br.readLine());
+
+            stairs = new Stair[2];
+            int temp;
+            int stairIndex = 0;
+            M = 0;
+            people = new ArrayList<>();
+            for(int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < W; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
+                for(int j = 0; j < N; j++) {
+                    temp = Integer.parseInt(st.nextToken());
+                    if(temp > 1) {
+                        stairs[stairIndex++] = new Stair(i, j, temp, 0);
+                    } else if(temp == 1) {
+                        people.add(new int[] {i, j});
+                    }
                 }
             }
 
-            /**
-             * 같은 특성의 약품을 연속으로 K개 투입시키면 무조건 성능검사 통과
-             * K를 ans에 대입시킨 후 이 값보다 약품 투입 횟수가 크면 탐색을 종료하게 함
-             */
-            ans = K;
-            solution(0, 0);
+            M = people.size();
+            ch = new boolean[M];
+
+            ans = Integer.MAX_VALUE;
+            subsets(0);
             sb.append(ans).append("\n");
         }
         System.out.println(sb);
     }
-    static void solution(int cnt, int curr) {
-        //현재까지의 최솟값보다 약품주입 횟수가 크거나 같으면 어차피 답 아니니깐 탐색 종료
-        if(curr >= ans) {
+
+    static void subsets(int cnt) {
+        if(cnt == M) {
+            ans = Math.min(ans, getTotalTime());
             return;
         }
 
-        //2-1. 성능 검사 통과하면, 최솟값 검증
-        if(isValid()) {
-            ans = curr; // 위에서 ans보다 작은 것 검증했으므로 무조건 대입해버림
-            return;
-        }
-
-        //종료조건
-        if(cnt == D) {
-            return;
-        }
-
-        //2-2. 성능 검사 통과 못하면, 약품 투입
-        int[] temp = Arrays.copyOf(map[cnt], W);
-        Arrays.fill(map[cnt], 0);
-        solution(cnt + 1, curr + 1);
-        Arrays.fill(map[cnt], 1);
-        solution(cnt + 1, curr + 1);
-        map[cnt] = Arrays.copyOf(temp, W);
-        solution(cnt + 1, curr);
+        ch[cnt] = true;
+        subsets(cnt + 1);
+        ch[cnt] = false;
+        subsets(cnt + 1);
     }
 
+    static int getTotalTime() {
+        LinkedList<Point> q = new LinkedList<>();
 
-    static boolean isValid() {
-        boolean flag;
-        int old, cnt;
-        for(int i = 0; i < W; i++) {
-            flag = false;
-            old = map[0][i];
-            cnt = 1;
-            for(int j = 1; j < D; j++) {
-                //동일 특성이 K개 연속되어있는지 확인
-                if(old == map[j][i]) {
-                    cnt++;
-                } else {
-                    cnt = 1;
-                    old = map[j][i];
-                }
-                //만약 K개를 충족하면 해당 열은 더이상 탐색할 필요없으므로 중단
-                if(cnt == K) {
-                    flag = true;
-                    break;
-                }
-            }
-            //만약 한 열을 탐색하면서 flag가 변경되지 않으면 성능검사 통과 못한것이므로 false 리턴
-            if(!flag) {
-                return false;
+        for(int i = 0; i < M; i++) {
+            //ch가 true면 0번 계단 선택으로 판단
+            int curr[] = people.get(i);
+            if(ch[i]) {
+                int dir = getDist(curr[0], curr[1], stairs[0].r, stairs[0].c);
+                q.offer(new Point(dir + 1, 0, stairs[0].len)); //계단 올라가도 한 칸 더 기다려야 되니깐!
+            } else {
+                int dir = getDist(curr[0], curr[1], stairs[1].r, stairs[1].c);
+                q.offer(new Point(dir + 1, 1, stairs[1].len));
             }
         }
-        return true;
+
+        int time = 0;
+        int n = M;
+        while(!q.isEmpty()) {
+            time++;
+            int a = 0;
+            int b = 0;
+            //계단에 있는 애들 처리
+            for(int i = 0; i < n; i++) {
+                Point p = q.get(i);
+                if(p.dir == 0) { //계단 도착
+                    if(p.len == stairs[p.n].len) { // 계단 만석
+                        continue;
+                    }
+                    if(0 < p.len  && p.len < stairs[p.n].len) { //p가 계단 내려가는중
+                        p.len -= 1;
+                    }
+                    if(p.len == 0) { //이번턴에 계단 끝 도착 나가기
+                        if(p.n == 0) {
+                            a++;
+                        } else {
+                            b++;
+                        }
+                        q.remove(p);
+                        i--;
+                        n--;
+                    }
+                }
+            }
+
+            for(int i = 0; i < n; i++) {
+                Point p = q.get(i);
+                if(p.dir > 0) {//아직 계단 못도착 이동
+                    p.dir -= 1;
+                }
+                //아까 계단 도착한 애들 중 들어갈 수 있으면 들어가기
+                else if(p.dir == 0 && p.len == stairs[p.n].len && stairs[p.n].cnt < 3) {
+                    stairs[p.n].cnt += 1;
+                    p.len -= 1;
+                }
+            }
+            stairs[0].cnt -= a;
+            stairs[1].cnt -= b;
+        }
+
+
+        return time;
+    }
+
+    static int getDist(int r1, int c1, int r2, int c2) {
+        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
     }
 }
+
